8abff49c0e4f3112bb06420eae077ae3048939a9
David Cramb <david.cramb@gmail.com>
2016-05-18 21:19:08 -0500

    added controller for navbar links

--- app/app.js
+++ app/app.js
@@ -1,1 +1,10 @@
-console.log('hello'); 
+var app = angular.module("TodoApp", []);
+
+app.controller("NavCtrl", function($scope){
+  $scope.navItems = [{name: "All Items"}, {name: "New Item"}];
+});
+//set up controller, inject variables using dollar-scope
+app.controller("TodoCtrl", function($scope) {
+  $scope.welcome = "hello";
+});
+
