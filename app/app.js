5c187dc7181f1114bd3ae35bf96add480d255ab4
David Cramb <david.cramb@gmail.com>
2016-05-21 10:11:55 -0500

    added show list view angular stuff

--- app/app.js
+++ app/app.js
@@ -1,10 +1,26 @@
 var app = angular.module("TodoApp", []);
 
 app.controller("NavCtrl", function($scope){
-  $scope.navItems = [{name: "All Items"}, {name: "New Item"}];
+  $scope.navItems = [{name: "Logout"}, {name: "New Item"} ,{name: "All Items"}];
 });
 //set up controller, inject variables using dollar-scope
 app.controller("TodoCtrl", function($scope) {
+
   $scope.welcome = "hello";
+  $scope.showListView = true;
+
+  $scope.newItem = function(){
+    console.log('New Item');
+    $scope.showListView = false
+  };
+  $scope.allItem = function() {
+    console.log('ALL');
+    $scope.showListView = true
+  };
+
+
+
+
+
 });
 
