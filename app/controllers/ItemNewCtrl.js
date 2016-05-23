3297328809901d0721a984b09bb0ab02d6c05f16
David Cramb <david.cramb@gmail.com>
2016-05-23 18:23:44 -0500

    added json file, deleted item array from controllers

--- app/controllers/ItemNewCtrl.js
+++ app/controllers/ItemNewCtrl.js
@@ -1,37 +1,7 @@
 app.controller("ItemNewCtrl", function($scope) {
   $scope.newTask = {};
-  $scope.items = [
-    {
-      id: 0,
-      task: "mow the lawn",
-      isCompleted: true,
-      dueDate: "12/5/17",
-      assignedTo: "greg",
-      location: "home",
-      urgency: "low",
-      dependencies: "wifi, tissues, vodka"
-    },
-    {
-      id: 1,
-      task: "grade quizzes zoe",
-      isCompleted: false,
-      dueDate: "12/5/16",
-      assignedTo: "Joe",
-      location: "NSS",
-      urgency: "high",
-      dependencies: "sunshine, clippers, hat, water, headphones"
-    },
-    {
-      id: 2,
-      task: "take a nap",
-      isCompleted: false,
-      dueDate: "5/21/16",
-      assignedTo: "zoe",
-      location: "Zoe's House",
-      urgency: "medium",
-      dependencies: "hammock, cat, pillow, blanket"
-    }
-  ];
+  $scope.items = [];
+
 
   $scope.addNewItem = function() {
     $scope.newTask.isCompleted = false;
